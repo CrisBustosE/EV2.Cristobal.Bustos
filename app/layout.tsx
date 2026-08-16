2599e2a11b9a62d839b966da8a915b89b5c90fb2
Cristobal Bustos <cris@ev2.dev>
2026-08-16 03:11:53 -0400

    chore: add dummy favicon and fix smooth scrolling behavior attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html lang="es" data-scroll-behavior="smooth">
       <head>
         <link rel="icon" href="data:," />
         <link
